eb0427ebf823f63160c2ca4216ccf0bfa613afce
David Weingut <da.we_2003@web.de>
2026-04-16 14:32:04 +0200

    add labels and titles to plots

--- code/L1/phasePortrait.py
+++ code/L1/phasePortrait.py
@@ -32,17 +32,21 @@ def plot_phase_portrait(x0y0, params, traj_length=100, trans=20):
     traj = get_trajectory(x0y0, params, traj_length, trans)
     plt.scatter(
         *traj.T,
-        c=np.array(range(traj_length)) / traj_length,
+        c=np.array(range(traj_length)),
         label=
         f"x0y0=({x0y0[0]:.3f}, {x0y0[1]:.3f}), (r1,r2,$\\epsilon$)={params}")
 
 
 def get_report_figures():
     trans_val = 30
+    plt.close()
     for param in [(2.8, 2.9, 0.1), (3.1, 3.4, 0.3), (3.85, 3.95, 0.2)]:
         plot_phase_portrait(np.random.rand(2), param, trans=trans_val)
         plot_phase_portrait(np.random.rand(2), param, trans=trans_val)
-        plt.colorbar()
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.suptitle(f"Phase Portrait for (r1, r2, $\\epsilon$)={param}")
+        plt.colorbar(label="Step")
         plt.savefig(f"figures/phase_{param}.pdf")
         plt.close()
 
