5a9b709c0f03bc1d304720dd50dc00da7797e65f
David Weingut <da.we_2003@web.de>
2026-04-23 11:40:14 +0200

    update phasePortrait to be more reproducible

--- code/L1/phasePortrait.py
+++ code/L1/phasePortrait.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from common import coupled_map
 import numpy as np
 
+rng = np.random.default_rng(42)
+
 default_params = (3.1, 3.1, 0.3)
 default_x0y0 = (0.3, 0.7)
 
@@ -36,11 +38,10 @@ def plot_phase_portrait(x0y0,
     traj = get_trajectory(x0y0, params, traj_length, trans)
     if should_print:
         print(traj[-1])
-    plt.scatter(
-        *traj.T,
-        c=np.array(range(traj_length)),
-        label=
-        f"x0y0=({x0y0[0]:.3f}, {x0y0[1]:.3f}), (r1,r2,$\\epsilon$)={params}")
+    plt.scatter(*traj.T,
+                c=np.array(range(traj_length)),
+                label=f"x0y0=({x0y0[0]:.3f}, {x0y0[1]:.3f})," +
+                f"(r1,r2,$\\epsilon$)={params}")
 
 
 def get_report_figures():
@@ -49,13 +50,12 @@ def get_report_figures():
     for param in [(2.8, 2.9, 0.1), (3.1, 3.4, 0.3), (3.85, 3.95, 0.2)]:
         print(f"\n{param=}")
         for _ in range(10):
-            plot_phase_portrait(np.random.rand(2),
+            plot_phase_portrait(rng.random(2),
                                 param,
                                 trans=trans_val,
                                 should_print=True)
 
         print()
-        #plot_phase_portrait(np.random.rand(2), param, trans=trans_val)
         plt.xlabel("x")
         plt.ylabel("y")
         plt.suptitle(f"Phase Portrait for (r1, r2, $\\epsilon$)={param}")
@@ -66,8 +66,8 @@ def get_report_figures():
 
 if __name__ == "__main__":
     trans_val = 20
-    plot_phase_portrait(np.random.rand(2), default_params, trans=trans_val)
-    plot_phase_portrait(np.random.rand(2), default_params, trans=trans_val)
+    plot_phase_portrait(rng.random(2), default_params, trans=trans_val)
+    plot_phase_portrait(rng.random(2), default_params, trans=trans_val)
     plt.legend()
     plt.colorbar()
     plt.show()
