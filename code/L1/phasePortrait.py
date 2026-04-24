9dc65d96e4c19c5af9247e607d585e48d501396b
David Weingut <da.we_2003@web.de>
2026-04-25 00:26:54 +0200

    extend phase portraits for comparison with bifurcation diagram

--- code/L1/phasePortrait.py
+++ code/L1/phasePortrait.py
@@ -47,7 +47,8 @@ def plot_phase_portrait(x0y0,
 def get_report_figures():
     trans_val = 30
     plt.close()
-    for param in [(2.8, 2.9, 0.1), (3.1, 3.4, 0.3), (3.85, 3.95, 0.2)]:
+    for param in [(2.8, 2.9, 0.1), (3.1, 3.4, 0.3), (3.85, 3.95, 0.2),
+                  (3.1, 3.1, 0.3), (3.1, 3.95, 0.3)]:
         print(f"\n{param=}")
         for _ in range(10):
             plot_phase_portrait(rng.random(2),
